832a07c3e965294809f16b1cb34af6c87b320823
Kristian Skov <kris701kj@gmail.com>
2023-09-19 13:06:19 +0200

    Added an initial CLI interface

---
+++ Toolchain/Options.cs
@@ -0,0 +1,36 @@
+﻿using CommandLine.Text;
+using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toolchain
+{
+    public class Options
+    {
+        [Option('b', "benchmark", Required = true, HelpText = "Path to the benchmark file to use")]
+        public string BennchmarkPath { get; set; } = "";
+        [Option('s', "samples", Required = true, HelpText = "How many samples are to be takes as the 'train' set")]
+        public int Samples { get; set; } = 0;
+
+        [Option('d', "deps", Required = false, HelpText = "Check if required dependencies are installed")]
+        public bool CheckDependencies { get; set; } = false;
+        [Option('e', "seed", Required = false, HelpText = "Optional seed for all the random number generation")]
+        public int Seed { get; set; } = -1;
+        [Option('m', "multithread", Required = false, HelpText = "If parts of the toolchain should run multithreaded or not.")]
+        public bool Multithread { get; set; } = false;
+
+        [Usage(ApplicationAlias = "Toolchain")]
+        public static IEnumerable<Example> Examples
+        {
+            get
+            {
+                return new List<Example>() {
+                        new Example("Run the toolchain on a benchmark file", new Options { BennchmarkPath = "depot.json", Samples = 2 })
+                      };
+            }
+        }
+    }
+}
--- Toolchain/Program.cs
+++ Toolchain/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using CommandLine;
+using CommandLine.Text;
+using PlanSampleGenerator;
+using System;
+using Tools;
+using Tools.Benchmarks;
 
 namespace Toolchain
 {
@@ -6,8 +11,62 @@ namespace Toolchain
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Toolchain started!");
+            Parser.Default.ParseArguments<Options>(args)
+              .WithParsed(RunOptions)
+              .WithNotParsed(HandleParseError);
+        }
 
+        static void HandleParseError(IEnumerable<Error> errs)
+        {
+            foreach (var error in errs)
+                ConsoleHelper.WriteLineColor($"{error}", ConsoleColor.Red);
+        }
+
+        static void RunOptions(Options opts)
+        {
+            if (!File.Exists(opts.BennchmarkPath))
+                throw new FileNotFoundException("The given benchmark file was not found!");
+
+            ConsoleHelper.WriteLineColor("Toolchain have started...", ConsoleColor.DarkGray);
+
+            var projectPath = ProjectHelper.GetProjectPath();
+
+            if (opts.CheckDependencies)
+                CheckDependencies();
+
+            var benchmark = ParseBenchmarkFile(opts.BennchmarkPath);
+
+            GeneratePlanSamples(benchmark, opts.Samples, opts.Seed, opts.Multithread, projectPath);
+
+            ConsoleHelper.WriteLineColor("Toolchain have finished!", ConsoleColor.DarkGray);
+
+        }
+
+        private static void CheckDependencies()
+        {
+            ConsoleHelper.WriteLineColor("Checking Dependencies...", ConsoleColor.DarkGray);
+            ConsoleHelper.WriteLineColor("Done!", ConsoleColor.Green);
+        }
+
+        private static Benchmark ParseBenchmarkFile(string path)
+        {
+            ConsoleHelper.WriteLineColor("Parsing benchmark file...", ConsoleColor.DarkGray);
+            var benchmarkFile = new Benchmark(path);
+            ConsoleHelper.WriteLineColor("Done!", ConsoleColor.Green);
+            return benchmarkFile;
+        }
+
+        private static void GeneratePlanSamples(Benchmark benchmark, int samples, int seed, bool multithread, string projectPath)
+        {
+            ConsoleHelper.WriteLineColor("Generating Plan Samples...", ConsoleColor.DarkGray);
+
+            IPlanFetcher fetcher = new FastDownwardPlanFetcher(
+                "python",
+                Path.Combine(projectPath, "Dependencies", "fast-downward", "fast-downward.py"),
+                "--alias lama-first");
+            fetcher.Fetch(benchmark, samples, multithread, seed);
+
+            ConsoleHelper.WriteLineColor("Done!", ConsoleColor.Green);
         }
     }
 }
