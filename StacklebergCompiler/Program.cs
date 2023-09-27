809d4bac22820d56c7fa1ea9e756e30bbdd81f5a
Kristian Skov <kris701kj@gmail.com>
2023-09-27 14:08:05 +0200

    Added initial project

---
+++ StacklebergCompiler/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using CommandLine;
+using System.Diagnostics;
+using System.Text;
+using PDDLSharp;
+using Tools;
+using PDDLSharp.Parsers;
+using PDDLSharp.ErrorListeners;
+using PDDLSharp.Models.Problem;
+using PDDLSharp.Models.Domain;
+
+namespace StacklebergCompiler
+{
+    internal class Program : BaseCLI
+    {
+        static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<StacklebergCompilerOptions>(args)
+              .WithParsed(RunStacklebergCompiler)
+              .WithNotParsed(HandleParseError);
+        }
+
+        public static void RunStacklebergCompiler(StacklebergCompilerOptions opts)
+        {
+            IErrorListener listener = new ErrorListener();
+            IPDDLParser parser = new PDDLParser(listener);
+
+            var domain = parser.ParseAs<DomainDecl>(opts.DomainFilePath);
+            var problem = parser.ParseAs<ProblemDecl>(opts.ProblemFilePath);
+            var metaAction = parser.ParseAs<ActionDecl>(opts.MetaActionFile);
+
+
+        }
+    }
+}
