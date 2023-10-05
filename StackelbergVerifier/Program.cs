eebdd7ea292de0141f87a71400dd6eeeb45f96c3
Kristian Skov <kris701kj@gmail.com>
2023-10-05 20:30:00 +0200

    Added a verifyer

---
+++ StackelbergVerifier/Program.cs
@@ -0,0 +1,83 @@
+﻿using CommandLine;
+using StackelbergVerifier;
+using System;
+using System.Diagnostics;
+using System.Text;
+using System.Xml.Linq;
+using Tools;
+
+namespace StacklebergVerifier
+{
+    internal class Program : BaseCLI
+    {
+        static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<StackelbergVerifierOptions>(args)
+              .WithNotParsed(HandleParseError)
+              .WithParsed(RunStacklebergVerifier);
+        }
+
+        public static void RunStacklebergVerifier(StackelbergVerifierOptions opts)
+        {
+            opts.OutputPath = PathHelper.RootPath(opts.OutputPath);
+            if (!Directory.Exists(opts.OutputPath))
+                Directory.CreateDirectory(opts.OutputPath);
+            opts.DomainFilePath = PathHelper.RootPath(opts.DomainFilePath);
+            opts.ProblemFilePath = PathHelper.RootPath(opts.ProblemFilePath);
+            opts.StackelbergPath = PathHelper.RootPath(opts.StackelbergPath);
+
+            StringBuilder sb = new StringBuilder("");
+            sb.Append($"{opts.StackelbergPath} ");
+            sb.Append($"\"{opts.DomainFilePath}\" ");
+            sb.Append($"\"{opts.ProblemFilePath}\" ");
+            if (opts.IsEasyProblem)
+                sb.Append($"--search \"sym_stackelberg(optimal_engine=symbolic(plan_reuse_minimal_task_upper_bound=false, plan_reuse_upper_bound=true), upper_bound_pruning=false)\" ");
+            else
+                sb.Append($"--search \"sym_stackelberg(optimal_engine=symbolic(plan_reuse_minimal_task_upper_bound=true, plan_reuse_upper_bound=true, force_bw_search_minimum_task_seconds=30, time_limit_seconds_minimum_task=300), upper_bound_pruning=true)\" ");
+
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo()
+                {
+                    FileName = opts.PythonPrefix,
+                    Arguments = sb.ToString(),
+                    CreateNoWindow = true,
+                    UseShellExecute = false,
+                    RedirectStandardError = true,
+                    RedirectStandardOutput = true,
+                    WorkingDirectory = opts.OutputPath
+                }
+            };
+
+            process.Start();
+            Console.WriteLine("Verifying...");
+            while(!process.HasExited)
+            {
+                Console.Write(".");
+                Thread.Sleep(1000);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Done!");
+            Console.WriteLine("Checking frontier...");
+            if (IsFrontierValid(Path.Combine(opts.OutputPath, "pareto_frontier.json")))
+                Console.WriteLine("== Frontier is valid ==");
+            else
+                Console.WriteLine("== Frontier is not valid ==");
+        }
+
+        private static bool IsFrontierValid(string file)
+        {
+            if (!File.Exists(file))
+                return false;
+            var text = File.ReadAllText(file);
+            var index = text.LastIndexOf("\"defender cost\": ") + "\"defender cost\": ".Length;
+            var endIndex = text.IndexOf(",", index);
+            var numberStr = text.Substring(index, endIndex - index);
+            var number = Int32.Parse(numberStr);
+            if (number != int.MaxValue)
+                return true;
+            return false;
+        }
+
+    }
+}
---
+++ StackelbergVerifier/StackelbergVerifierOptions.cs
@@ -0,0 +1,25 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackelbergVerifier
+{
+    public class StackelbergVerifierOptions
+    {
+        [Option("domain", Required = true, HelpText = "Path to the domain file to use")]
+        public string DomainFilePath { get; set; } = "";
+        [Option("problem", Required = true, HelpText = "Path to the problem file to use")]
+        public string ProblemFilePath { get; set; } = "";
+        [Option("output", Required = false, HelpText = "Path to output files to")]
+        public string OutputPath { get; set; } = "";
+        [Option("py", Required = false, HelpText = "What python prefix to use", Default = "python2")]
+        public string PythonPrefix { get; set; } = "python2";
+        [Option("stackelberg", Required = false, HelpText = "Path to stackelberg planner", Default = "fast-downward.py")]
+        public string StackelbergPath { get; set; } = "fast-downward.py";
+        [Option("iseasy", Required = false, HelpText = "Is the problem an easy one", Default = true)]
+        public bool IsEasyProblem { get; set; } = true;
+    }
+}
