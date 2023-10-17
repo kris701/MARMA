59a49e744395006fd526797c931f283734d45fc9
Jan M. D. Hansen <jan.m.d.hansen@protonmail.com>
2023-10-17 15:55:55 +0200

    fix: removes out of bounds error on stiching

--- reconstruction/reconstruction/src/stiching.rs
+++ reconstruction/reconstruction/src/stiching.rs
@@ -1,26 +1,99 @@
-use spingus::{sas_plan::SASPlan, term::Term};
+use spingus::sas_plan::SASPlan;
 
-pub fn stich(plan: SASPlan, replacements: Vec<(&usize, &SASPlan)>) -> SASPlan {
+pub fn stich(plan: &SASPlan, replacements: Vec<(&usize, &SASPlan)>) -> SASPlan {
     if replacements.is_empty() {
-        return plan;
-    }
-    let mut steps: Vec<Term> = vec![];
-
-    let mut next_replacement: Option<usize> = Some(0);
-    for i in 0..plan.len() {
-        if next_replacement.is_some() && i == *replacements[next_replacement.unwrap()].0 {
-            for step in replacements[next_replacement.unwrap()].1 {
-                steps.push(step.clone());
-            }
-            if next_replacement.unwrap() == replacements.len() {
-                next_replacement = None;
-            } else {
-                next_replacement = Some(next_replacement.unwrap() + 1);
-            }
-        } else {
-            steps.push(plan[i].clone());
-        }
+        return plan.clone();
     }
 
-    steps
+    let mut stiched_plan = plan.to_owned();
+
+    replacements.iter().rev().for_each(|(i, plan)| {
+        stiched_plan.remove(**i);
+        stiched_plan.splice(**i..**i, plan.iter().cloned());
+    });
+
+    stiched_plan
+}
+
+#[cfg(test)]
+mod test {
+    use spingus::{
+        sas_plan::{export_sas, parse_sas},
+        term::Term,
+    };
+
+    use super::stich;
+
+    #[test]
+    fn stitch_none() {
+        let meta_plan = "(a)\n(b)\n(c)\n";
+        let parsed_plan = parse_sas(meta_plan).unwrap();
+        let replacements = vec![];
+
+        let stiched = stich(&parsed_plan, replacements);
+        let stiched_s = export_sas(&stiched);
+
+        assert_eq!(meta_plan, stiched_s);
+    }
+
+    #[test]
+    fn stitch_first() {
+        let meta_plan = vec![
+            Term {
+                name: "a".to_string(),
+                parameters: vec![],
+            },
+            Term {
+                name: "b".to_string(),
+                parameters: vec![],
+            },
+            Term {
+                name: "c".to_string(),
+                parameters: vec![],
+            },
+        ];
+        let index = 0 as usize;
+        let replacement_plan = vec![
+            Term {
+                name: "1".to_string(),
+                parameters: vec![],
+            },
+            Term {
+                name: "2".to_string(),
+                parameters: vec![],
+            },
+            Term {
+                name: "3".to_string(),
+                parameters: vec![],
+            },
+        ];
+
+        let stiched = stich(&meta_plan, vec![(&index, &replacement_plan)]);
+
+        assert_eq!(
+            vec![
+                Term {
+                    name: "1".to_string(),
+                    parameters: vec![],
+                },
+                Term {
+                    name: "2".to_string(),
+                    parameters: vec![],
+                },
+                Term {
+                    name: "3".to_string(),
+                    parameters: vec![],
+                },
+                Term {
+                    name: "b".to_string(),
+                    parameters: vec![],
+                },
+                Term {
+                    name: "c".to_string(),
+                    parameters: vec![],
+                },
+            ],
+            stiched
+        );
+    }
 }
