90c37e6604ab87b13afa8fa028f99510d96b4a9d
Kristian Skov Johansen <kris701kj@gmail.com>
2023-10-17 13:15:39 +0200

    Functional version

--- MetaActions.Learn/Program.cs
+++ MetaActions.Learn/Program.cs
@@ -28,20 +28,26 @@ namespace MetaActions.Learn
                 new DirectoryInfo(opts.OutputPath).Delete(true);
             Directory.CreateDirectory(opts.OutputPath);
 
+            var problemsPath = Path.Combine(opts.TempPath, "problems");
+            var problems = new List<FileInfo>();
+            Directory.CreateDirectory(problemsPath);
+            foreach (var problem in opts.Problems) 
+            {
+                var file = new FileInfo(PathHelper.RootPath(problem));
+                File.Copy(file.FullName, Path.Combine(opts.TempPath, "problems", file.Name));
+                problems.Add(new FileInfo(Path.Combine(opts.TempPath, "problems", file.Name)));
+            }
+
             ConsoleHelper.WriteLineColor($"Generating macros", ConsoleColor.Gray);
-            List<FileInfo> allMacros = GenerateMacros(opts.DomainPath, opts.ProblemsPath, opts.TempPath);
-            if (Failed)
-                return;
+            Directory.CreateDirectory(Path.Combine(opts.TempPath, "macros"));
+            List<FileInfo> allMacros = GenerateMacros(opts.DomainPath, Path.Combine(opts.TempPath, "problems"), opts.TempPath);
 
             ConsoleHelper.WriteLineColor($"Generating meta actions", ConsoleColor.Gray);
             List<FileInfo> allMetaActions = GenerateMetaActions(opts.DomainPath, opts.TempPath);
-            if (Failed)
-                return;
 
             ConsoleHelper.WriteLineColor($"Testing meta actions", ConsoleColor.Gray);
             int totalValidMetaActions = 0;
             int metaActionCounter = 1;
-            var problems = new DirectoryInfo(opts.ProblemsPath).GetFiles().ToList();
             foreach (var metaAction in allMetaActions)
             {
                 int problemCounter = 1;
@@ -49,7 +55,7 @@ namespace MetaActions.Learn
                 isValid = false;
                 foreach (var problem in problems)
                 {
-                    ConsoleHelper.WriteLineColor($"Testing meta action {metaActionCounter} on problem {problemCounter++} out of {problems.Count}.", ConsoleColor.Gray);
+                    ConsoleHelper.WriteLineColor($"Testing meta action {metaActionCounter} of {allMetaActions.Count} on problem {problemCounter++} out of {problems.Count}.", ConsoleColor.Gray);
                     // Compile Meta Actions
                     ConsoleHelper.WriteLineColor($"Compiling meta action.", ConsoleColor.Gray);
                     ArgsCaller stackelCompiler = GetDotnetRunner("StacklebergCompiler");
@@ -93,75 +99,6 @@ namespace MetaActions.Learn
                 metaActionCounter++;
             }
             ConsoleHelper.WriteLineColor($"A total of {totalValidMetaActions} valid meta actions was found.", ConsoleColor.Green);
-
-            //ConsoleHelper.WriteLineColor($"A total of {opts.TrainProblems.Count()} problems to train on.", ConsoleColor.Gray);
-            //int problemCounter = 1;
-            //int totalValidMetaActions = 0;
-            //foreach (var problem in opts.TrainProblems)
-            //{
-            //    ConsoleHelper.WriteLineColor($"Training on problem '{new FileInfo(problem).Name}' started [{problemCounter++} of {opts.TrainProblems.Count()}]", ConsoleColor.Gray);
-            //    // Purge temp folder
-            //    new DirectoryInfo(opts.TempPath).Delete(true);
-
-            //    var rootedProblem = PathHelper.RootPath(problem);
-
-            //    // Generate Meta Actions
-            //    ConsoleHelper.WriteLineColor($"Generating meta actions", ConsoleColor.Gray);
-            //    ArgsCaller metaCaller = GetDotnetRunner("MetaActionGenerator");
-            //    metaCaller.Arguments.Add("--domain", opts.DomainPath);
-            //    metaCaller.Arguments.Add("--macros", Path.Combine(opts.TempPath, "macros"));
-            //    metaCaller.Arguments.Add("--output", Path.Combine(opts.TempPath, "metaActions"));
-            //    metaCaller.Run();
-            //    if (Failed)
-            //        return;
-            //    var allMetaActions = new DirectoryInfo(Path.Combine(opts.TempPath, "metaActions")).GetFiles();
-            //    ConsoleHelper.WriteLineColor($"A total of {allMetaActions.Length} meta actions found.", ConsoleColor.Gray);
-
-            //    int counter = 1;
-            //    foreach (var metaAction in allMetaActions)
-            //    {
-            //        ConsoleHelper.WriteLineColor($"Testing meta action {counter++} out of {allMetaActions.Length}.", ConsoleColor.Gray);
-            //        // Compile Meta Actions
-            //        ConsoleHelper.WriteLineColor($"Compiling meta action.", ConsoleColor.Gray);
-            //        ArgsCaller stackelCompiler = GetDotnetRunner("StacklebergCompiler");
-            //        stackelCompiler.Arguments.Add("--domain", opts.DomainPath);
-            //        stackelCompiler.Arguments.Add("--problem", rootedProblem);
-            //        stackelCompiler.Arguments.Add("--meta-action", metaAction.FullName);
-            //        stackelCompiler.Arguments.Add("--output", Path.Combine(opts.TempPath, "compiled"));
-            //        stackelCompiler.Run();
-            //        if (Failed)
-            //            return;
-
-            //        // Verify Meta Actions
-            //        ConsoleHelper.WriteLineColor($"Verifying meta action.", ConsoleColor.Gray);
-            //        ArgsCaller stackelVerifier = GetDotnetRunner("StackelbergVerifier");
-            //        stackelVerifier.StdOut -= PrintStdOut;
-            //        stackelVerifier.StdOut += PrintStdOutVerifier;
-            //        stackelVerifier.Arguments.Add("--domain", Path.Combine(opts.TempPath, "compiled", "simplified_domain.pddl"));
-            //        stackelVerifier.Arguments.Add("--problem", Path.Combine(opts.TempPath, "compiled", "simplified_problem.pddl"));
-            //        stackelVerifier.Arguments.Add("--output", Path.Combine(opts.TempPath, "verification"));
-            //        stackelVerifier.Arguments.Add("--stackelberg", "Dependencies/stackelberg-planner/src/fast-downward.py");
-            //        stackelVerifier.Run();
-            //        if (Failed)
-            //            return;
-
-            //        // Output Valid Meta Actions
-            //        if (!Directory.Exists(Path.Combine(opts.OutputPath, "valid")))
-            //            Directory.CreateDirectory(Path.Combine(opts.OutputPath, "valid"));
-            //        if (isValid)
-            //        {
-            //            ConsoleHelper.WriteLineColor($"Meta action was valid.", ConsoleColor.Green);
-            //            totalValidMetaActions++;
-            //            File.WriteAllText(Path.Combine(opts.OutputPath, "valid", $"meta{counter}.pddl"), File.ReadAllText(metaAction.FullName));
-            //        }
-            //        else
-            //        {
-            //            ConsoleHelper.WriteLineColor($"Meta action was invalid.", ConsoleColor.Red);
-            //        }
-            //        isValid = false;
-            //    }
-            //}
-            //ConsoleHelper.WriteLineColor($"A total of {totalValidMetaActions} valid meta actions was found.", ConsoleColor.Green);
         }
 
         private static List<FileInfo> GenerateMacros(string domain, string problems, string tempPath)
@@ -170,8 +107,8 @@ namespace MetaActions.Learn
             macroGenerator.Arguments.Add("-d", domain);
             macroGenerator.Arguments.Add("-p", problems);
             macroGenerator.Arguments.Add("-o", Path.Combine(tempPath, "macros"));
-            macroGenerator.Arguments.Add("-c", "Dependencies/CSMs/src");
-            macroGenerator.Arguments.Add("-f", "Dependencies/fast-downward/fast-downward.py");
+            macroGenerator.Arguments.Add("-c", PathHelper.RootPath("Dependencies/CSMs"));
+            macroGenerator.Arguments.Add("-f", PathHelper.RootPath("Dependencies/fast-downward/fast-downward.py"));
             macroGenerator.Run();
             return new DirectoryInfo(Path.Combine(tempPath, "macros")).GetFiles().ToList();
         }
@@ -188,6 +125,7 @@ namespace MetaActions.Learn
 
         private static ArgsCaller GetDotnetRunner(string project)
         {
+            Failed = false;
             ArgsCaller runner = new ArgsCaller("dotnet");
             runner.StdOut += PrintStdOut;
             runner.StdErr += PrintStdErr;
@@ -200,12 +138,13 @@ namespace MetaActions.Learn
 
         private static ArgsCaller GetRustRunner(string project)
         {
+            Failed = false;
             ArgsCaller runner = new ArgsCaller("cargo");
             runner.StdOut += PrintStdOut;
             runner.StdErr += PrintStdErr;
             runner.Arguments.Add("run", "");
             runner.Arguments.Add("--release", "");
-            runner.Arguments.Add("--manifest-path", $"{project}/Cargo.toml");
+            runner.Arguments.Add("--manifest-path", $"{project}/Cargo.toml --");
             return runner;
         }
 
