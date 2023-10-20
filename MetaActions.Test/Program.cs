d921c965df375345e39df7668a1b2434903b91e1
Kristian Skov Johansen <kris701kj@gmail.com>
2023-10-20 15:05:41 +0200

    Initial testing setup

--- MetaActions.Test/Program.cs
+++ MetaActions.Test/Program.cs
@@ -7,6 +7,7 @@ using PDDLSharp.Models.PDDL.Domain;
 using PDDLSharp.Parsers;
 using PDDLSharp.Parsers.PDDL;
 using System.Diagnostics;
+using System.IO.Compression;
 using Tools;
 
 namespace MetaActions.Test
@@ -20,116 +21,87 @@ namespace MetaActions.Test
               .WithNotParsed(HandleParseError);
         }
 
+        private static string _tempDataPath = "data";
+
         private static void Run(Options opts)
         {
-            ConsoleHelper.WriteLineColor($"Starting meta action testing", ConsoleColor.Blue);
+            if (opts.Rebuild)
+            {
+                ConsoleHelper.WriteLineColor($"Rebuilding toolchain...", ConsoleColor.Blue);
+                ArgsCallerBuilder.GetRustBuilder("reconstruction").Run();
+                ConsoleHelper.WriteLineColor($"Done!", ConsoleColor.Green);
+            }
 
-            opts.DomainPath = PathHelper.RootPath(opts.DomainPath);
-            opts.MetaActionsPath = PathHelper.RootPath(opts.MetaActionsPath);
             opts.TempPath = PathHelper.RootPath(opts.TempPath);
             opts.OutputPath = PathHelper.RootPath(opts.OutputPath);
+            opts.DataFile = PathHelper.RootPath(opts.DataFile);
 
-            IErrorListener listener = new ErrorListener();
-            IParser<INode> parser = new PDDLParser(listener);
-            ICodeGenerator<INode> codeGenerator = new PDDLCodeGenerator(listener);
-            DomainDecl domainDecl = parser.ParseAs<DomainDecl>(new FileInfo(opts.DomainPath));
-            List<ActionDecl> metaActions = new List<ActionDecl>();
-            foreach (var file in new DirectoryInfo(opts.MetaActionsPath).GetFiles())
-                metaActions.Add(parser.ParseAs<ActionDecl>(file));
+            //PathHelper.RecratePath(opts.TempPath);
+            PathHelper.RecratePath(opts.OutputPath);
 
-            RecratePath(opts.TempPath);
-            RecratePath(opts.OutputPath);
+            ConsoleHelper.WriteLineColor($"Extracting testing data", ConsoleColor.Blue);
+            _tempDataPath = Path.Combine(opts.TempPath, _tempDataPath);
+            //ZipFile.ExtractToDirectory(opts.DataFile, _tempDataPath);
+            ConsoleHelper.WriteLineColor($"Done!", ConsoleColor.Green);
 
-            ConsoleHelper.WriteLineColor($"There is a total of {opts.TestingProblems.Count()} problems to test with.", ConsoleColor.Blue);
+            ConsoleHelper.WriteLineColor($"Testing suite started", ConsoleColor.Blue);
 
-            int count = 1;
-            float totalNormalTime = 0;
-            float totalMetaTime = 0;
-            foreach (var problem in opts.TestingProblems)
+            List<Task<RunReport>> runTasks = new List<Task<RunReport>>();
+            foreach(var domain in new DirectoryInfo(_tempDataPath).GetDirectories())
             {
-                ConsoleHelper.WriteLineColor($"\tTesting problem {count++} out of {opts.TestingProblems.Count()}", ConsoleColor.Magenta);
-                var rootedProblem = PathHelper.RootPath(problem);
-                var name = new FileInfo(problem).Name.Replace(".pddl", "");
-                var outPath = Path.Combine(opts.OutputPath, name);
-                var tempPath = Path.Combine(opts.TempPath, name);
-                RecratePath(outPath);
-                RecratePath(tempPath);
-                ConsoleHelper.WriteLineColor($"\tExecuting normal problem", ConsoleColor.Magenta);
-                var normalTime = ExecuteAsNormal(opts.DomainPath, rootedProblem, Path.Combine(outPath, "normalPlan.plan"), Path.Combine(tempPath, "normalOutput.sas"));
-                ConsoleHelper.WriteLineColor($"\tExecuting meta problem", ConsoleColor.Magenta);
-                var metaTime = ExecuteAsMeta(domainDecl.Copy(), opts.DomainPath, rootedProblem, metaActions, tempPath, outPath, codeGenerator);
-                PrintResult(normalTime, metaTime);
-                totalNormalTime += normalTime;
-                totalMetaTime += metaTime;
+                var domainName = domain.Name;
+
+                PathHelper.RecratePath(Path.Combine(opts.TempPath, domainName));
+                PathHelper.RecratePath(Path.Combine(opts.OutputPath, domainName));
+
+                var normalDomain = new FileInfo(Path.Combine(domain.FullName, "domain.pddl"));
+                var metaDomain = new FileInfo(Path.Combine(domain.FullName, "metaDomain.pddl"));
+
+                var allProblems = new DirectoryInfo(domain.FullName).GetFiles().ToList();
+                allProblems.RemoveAll(x => x.Name == normalDomain.Name);
+                allProblems.RemoveAll(x => x.Name == metaDomain.Name);
+
+                foreach (var problem in allProblems)
+                {
+                    var problemName = problem.Name.Replace(".pddl","");
+                    runTasks.Add(new Task<RunReport>(() => new TestingTask().RunTest(
+                        normalDomain.FullName,
+                        "",
+                        problem.FullName,
+                        Path.Combine(opts.OutputPath, domainName, $"{problemName}.plan"),
+                        "",
+                        Path.Combine(opts.TempPath, domainName, $"{problemName}.sas")
+                        )));
+                    runTasks.Add(new Task<RunReport>(() => new TestingTask().RunTest(
+                        normalDomain.FullName,
+                        metaDomain.FullName,
+                        problem.FullName,
+                        Path.Combine(opts.OutputPath, domainName, $"reconstructed_{problemName}.plan"),
+                        Path.Combine(opts.OutputPath, domainName, $"meta_{problemName}.plan"),
+                        Path.Combine(opts.TempPath, domainName, $"{problemName}.sas")
+                        )));
+                }
             }
-            ConsoleHelper.WriteLineColor($"Testing finished!", ConsoleColor.Green);
-        }
 
-        private static void RecratePath(string path)
-        {
-            if (Directory.Exists(path))
-                new DirectoryInfo(path).Delete(true);
-            Directory.CreateDirectory(path);
-        }
+            try
+            {
+                Parallel.ForEach(runTasks, task => task.Start());
+                int preCount = runTasks.Count;
+                while (runTasks.Count > 0)
+                {
+                    var task = Task.WhenAny(runTasks).Result;
+                    runTasks.Remove(task);
+                    ConsoleHelper.WriteLineColor($"Training for problem {task.Result.Problem} complete! [{Math.Round(100 - (100 * ((double)runTasks.Count / (double)preCount)), 0)}%]", ConsoleColor.Green);
+                }
+            }
+            catch (Exception ex)
+            {
+                ConsoleHelper.WriteLineColor($"Something failed in the testing!", ConsoleColor.Red);
+                ConsoleHelper.WriteLineColor(ex.Message, ConsoleColor.Red);
+                return;
+            }
 
-        private static float ExecuteAsNormal(string domain, string problem, string planName, string sasName)
-        {
-            Stopwatch timer = new Stopwatch();
-            timer.Start();
-
-            ArgsCaller fdCaller = ArgsCallerBuilder.GetGenericRunner("python3");
-            fdCaller.Arguments.Add(PathHelper.RootPath("Dependencies/fast-downward/fast-downward.py"), "");
-            fdCaller.Arguments.Add("--alias", "lama-first");
-            fdCaller.Arguments.Add("--plan-file", planName);
-            fdCaller.Arguments.Add("--sas-file", sasName);
-            fdCaller.Arguments.Add(domain, "");
-            fdCaller.Arguments.Add(problem, "");
-            fdCaller.Run();
-
-            timer.Stop();
-            return timer.ElapsedMilliseconds;
-        }
-
-        private static float ExecuteAsMeta(DomainDecl domain, string originalDomain, string problem, List<ActionDecl> metaActions, string tempPath, string outPath, ICodeGenerator<INode> codeGenerator)
-        {
-            Stopwatch timer = new Stopwatch();
-            timer.Start();
-            // Reformulate domain
-            foreach (var act in metaActions)
-                domain.Actions.Add(act);
-            codeGenerator.Generate(domain, Path.Combine(tempPath, "reformulated_domain.pddl"));
-
-            // Execute with FD
-            ExecuteAsNormal(Path.Combine(tempPath, "reformulated_domain.pddl"), problem, Path.Combine(outPath, "metaPlan.plan"), Path.Combine(tempPath, "metaOutput.sas"));
-
-            // Reconstruct plan
-            ArgsCaller reconstructionFixer = ArgsCallerBuilder.GetRustRunner("reconstruction");
-            reconstructionFixer.Arguments.Add("-d", originalDomain);
-            reconstructionFixer.Arguments.Add("-p", problem);
-            reconstructionFixer.Arguments.Add("-m", Path.Combine(tempPath, "reformulated_domain.pddl"));
-            reconstructionFixer.Arguments.Add("-s", Path.Combine(outPath, "metaPlan.plan"));
-            reconstructionFixer.Arguments.Add("-f", PathHelper.RootPath("Dependencies/fast-downward/fast-downward.py"));
-            reconstructionFixer.Arguments.Add("-o", Path.Combine(outPath, "reconstructedPlan.plan"));
-            reconstructionFixer.Run();
-
-            timer.Stop();
-            return timer.ElapsedMilliseconds;
-        }
-
-        private static void PrintResult(float normalTime, float metaTime)
-        {
-            ConsoleHelper.WriteColor($"\tNormal took: ", ConsoleColor.Magenta);
-            if (normalTime < metaTime)
-                ConsoleHelper.WriteColor($"{normalTime}", ConsoleColor.Green);
-            else
-                ConsoleHelper.WriteColor($"{normalTime}", ConsoleColor.Red);
-            ConsoleHelper.WriteLineColor($"ms", ConsoleColor.Magenta);
-            ConsoleHelper.WriteColor($"\tMeta took:   ", ConsoleColor.Magenta);
-            if (normalTime < metaTime)
-                ConsoleHelper.WriteColor($"{metaTime}", ConsoleColor.Red);
-            else
-                ConsoleHelper.WriteColor($"{metaTime}", ConsoleColor.Green);
-            ConsoleHelper.WriteLineColor($"ms", ConsoleColor.Magenta);
+            ConsoleHelper.WriteLineColor($"Testing suite finished!", ConsoleColor.Green);
         }
     }
 }
---
+++ MetaActions.Test/TestingTask.cs
@@ -0,0 +1,86 @@
+﻿using PDDLSharp.CodeGenerators;
+using PDDLSharp.Models.PDDL.Domain;
+using PDDLSharp.Models.PDDL;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tools;
+
+namespace MetaActions.Test
+{
+    public class TestingTask
+    {
+        public RunReport RunTest(string domain, string metaDomain, string problem, string planName, string metaPlan, string sasName)
+        {
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+            if (metaDomain != "")
+            {
+                ExecuteAsMeta(domain, metaDomain, problem, planName, metaPlan, sasName);
+            }
+            else
+            {
+                ExecuteAsNormal(domain, problem, planName, sasName);
+            }
+            timer.Stop();
+
+            //var searchStart = _log.LastIndexOf("] Search time: ") + "] Search time: ".Length;
+            //var searchEnd = _log.IndexOf("s", searchStart);
+            //var timeStr = _log.Substring(searchStart, searchEnd - searchStart);
+            //double searchTime = double.Parse(timeStr);
+            var searchTime = 0;
+
+            if (metaDomain != "")
+                return new RunReport(metaDomain, problem, searchTime, timer.ElapsedMilliseconds);
+            else
+                return new RunReport(domain, problem, searchTime, timer.ElapsedMilliseconds);
+        }
+
+        private static string _log = "";
+        private static void ExecuteAsNormal(string domain, string problem, string planName, string sasName)
+        {
+            ArgsCaller fdCaller = new ArgsCaller("python3");
+            fdCaller.StdOut += LogStdOut;
+            fdCaller.StdErr += StopOnError;
+            fdCaller.Arguments.Add(PathHelper.RootPath("Dependencies/fast-downward/fast-downward.py"), "");
+            fdCaller.Arguments.Add("--alias", "lama-first");
+            fdCaller.Arguments.Add("--plan-file", planName);
+            fdCaller.Arguments.Add("--sas-file", sasName);
+            fdCaller.Arguments.Add(domain, "");
+            fdCaller.Arguments.Add(problem, "");
+            fdCaller.Run();
+        }
+
+        private static void LogStdOut(object sender, DataReceivedEventArgs e)
+        {
+            if (e.Data != null)
+                _log += $"{e.Data}{Environment.NewLine}";
+        }
+
+        private static void StopOnError(object sender, DataReceivedEventArgs e)
+        {
+            //if (e.Data != null)
+            //    throw new Exception($"An error occured: {e.Data}");
+        }
+
+        private static void ExecuteAsMeta(string domain, string metaDomain, string problem, string planName, string metaPlan, string sasName)
+        {
+            // Execute with FD
+            ExecuteAsNormal(metaDomain, problem, metaPlan, sasName);
+
+            // Reconstruct plan
+            ArgsCaller reconstructionFixer = ArgsCallerBuilder.GetRustRunner("reconstruction");
+            reconstructionFixer.Arguments.Add("-d", domain);
+            reconstructionFixer.Arguments.Add("-p", problem);
+            reconstructionFixer.Arguments.Add("-m", metaDomain);
+            reconstructionFixer.Arguments.Add("-s", metaPlan);
+            reconstructionFixer.Arguments.Add("-f", PathHelper.RootPath("Dependencies/fast-downward/fast-downward.py"));
+            reconstructionFixer.Arguments.Add("-o", planName);
+            if (reconstructionFixer.Run() != 0)
+                throw new Exception("Reconstruction failed!");
+        }
+    }
+}
