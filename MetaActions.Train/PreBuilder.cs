21daf8357843ca9df3b642bbe92492023f8fc957
Kristian Skov Johansen <kris701kj@gmail.com>
2023-10-21 12:23:22 +0200

    Additional messages

--- MetaActions.Train/PreBuilder.cs
+++ MetaActions.Train/PreBuilder.cs
@@ -12,13 +12,13 @@ namespace MetaActions.Learn
         public static void BuildToolchain()
         {
             if (ArgsCallerBuilder.GetDotnetBuilder("MetaActionGenerator").Run() != 0)
-                throw new Exception("Build failed!");
+                throw new Exception("'MetaActionGenerator' Build failed!");
             if (ArgsCallerBuilder.GetDotnetBuilder("StacklebergCompiler").Run() != 0)
-                throw new Exception("Build failed!");
+                throw new Exception("'StacklebergCompiler' Build failed!");
             if (ArgsCallerBuilder.GetDotnetBuilder("StackelbergVerifier").Run() != 0)
-                throw new Exception("Build failed!");
+                throw new Exception("'StackelbergVerifier' Build failed!");
             if (ArgsCallerBuilder.GetRustBuilder("macros").Run() != 0)
-                throw new Exception("Build failed!");
+                throw new Exception("'macros' Build failed!");
 
         }
     }
