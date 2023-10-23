32c39d30f4857ebfb013ce521fe55dfacf03a077
Kristian Skov Johansen <kris701kj@gmail.com>
2023-10-23 18:00:40 +0200

    Simplified the macro extractor

--- MacroExtractor/Program.cs
+++ MacroExtractor/Program.cs
@@ -1,6 +1,7 @@
 ﻿using CommandLine;
 using PDDLSharp.CodeGenerators;
 using PDDLSharp.CodeGenerators.PDDL;
+using PDDLSharp.CodeGenerators.Plans;
 using PDDLSharp.ErrorListeners;
 using PDDLSharp.Models.PDDL;
 using PDDLSharp.Models.PDDL.Domain;
@@ -8,8 +9,10 @@ using PDDLSharp.Models.PDDL.Expressions;
 using PDDLSharp.Models.Plans;
 using PDDLSharp.Parsers;
 using PDDLSharp.Parsers.PDDL;
+using PDDLSharp.Parsers.Plans;
 using PDDLSharp.Toolkit.MacroGenerators;
 using System;
+using System.Text.RegularExpressions;
 using System.Xml.Linq;
 using Tools;
 
@@ -30,90 +33,72 @@ namespace MacroExtractor
             opts.OutputPath = PathHelper.RootPath(opts.OutputPath);
             PathHelper.RecratePath(opts.OutputPath);
 
-            ConsoleHelper.WriteLineColor("Parsing plans...");
+            ConsoleHelper.WriteLineColor("Matching plans...");
+            List<FileInfo> leaderPlans = PathHelper.ResolveWildcards(opts.LeaderPlans.ToList());
+            List<FileInfo> followerPlans = PathHelper.ResolveWildcards(opts.FollowerPlans.ToList());
 
-            List<FileInfo> leaderPlans = new List<FileInfo>();
-            foreach (var plan in opts.LeaderPlans)
-                leaderPlans.Add(new FileInfo(PathHelper.RootPath(plan)));
-            List<FileInfo> followerPlans = new List<FileInfo>();
-            foreach (var plan in opts.FollowerPlans)
-                followerPlans.Add(new FileInfo(PathHelper.RootPath(plan)));
-
-            List<PlanPair> planPairs = new List<PlanPair>();
+            Dictionary<FileInfo, List<FileInfo>> leaderFollowerPairs = new Dictionary<FileInfo, List<FileInfo>>();
             foreach(var leaderPlan in leaderPlans)
             {
-                var matches = followerPlans.Where(x => x.Name == leaderPlan.Name);
-                foreach (var match in matches)
-                    planPairs.Add(new PlanPair(leaderPlan, match));
+                var leaderName = leaderPlan.Name.Replace(".plan","");
+                leaderFollowerPairs.Add(leaderPlan, new List<FileInfo>());
+                foreach (var followerPlan in followerPlans)
+                    if (followerPlan.Name.Replace(".plan", "").StartsWith($"{leaderName}_"))
+                        leaderFollowerPairs[leaderPlan].Add(followerPlan);
             }
             ConsoleHelper.WriteLineColor("Done!", ConsoleColor.Green);
 
-            ConsoleHelper.WriteLineColor("Parsing domain...");
-            var domain = ParseDomain(opts.Domain);
-            ConsoleHelper.WriteLineColor("Done!", ConsoleColor.Green);
-
             ConsoleHelper.WriteLineColor("Extracting reconstruction data...");
-            List<ReconstructionPair> macros = new List<ReconstructionPair>();
-            SimpleActionCombiner combiner = new SimpleActionCombiner();
-            foreach (var pair in planPairs)
+            Dictionary<GroundedAction, List<List<GroundedAction>>> macros = new Dictionary<GroundedAction, List<List<GroundedAction>>>();
+            IErrorListener listener = new ErrorListener();
+            IParser<ActionPlan> parser = new FastDownwardPlanParser(listener);
+            foreach (var leaderPlanFile in leaderFollowerPairs.Keys)
             {
-                int index = 0;
-                for(int i = 0; i < pair.FollowerPlan.Plan.Count; i++)
+                var leaderPlan = parser.Parse(leaderPlanFile);
+                int index = IndexOfMetaAction(leaderPlan);
+                var metaAction = leaderPlan.Plan[index];
+                foreach (var arg in metaAction.Arguments)
+                    arg.Name = $"?{arg}";
+                foreach (var followerPlanFile in leaderFollowerPairs[leaderPlanFile])
                 {
-                    if (!pair.FollowerPlan.Plan[i].Equals(pair.LeaderPlan.Plan[i]))
-                    {
-                        index = i;
-                        break;
-                    }
-                }
-                var metaAction = pair.LeaderPlan.Plan[index];
-                var groundedMacroSequence = pair.FollowerPlan.Plan.GetRange(index, pair.FollowerPlan.Plan.Count - index);
-                var macroSequence = GenerateSequence(groundedMacroSequence, domain);
-                var macro = combiner.Combine(macroSequence);
+                    var followerPlan = parser.Parse(followerPlanFile);
+                    var groundedMacroSequence = followerPlan.Plan.GetRange(index, followerPlan.Plan.Count - index);
+                    foreach (var groundedSequence in groundedMacroSequence)
+                        foreach (var arg in groundedSequence.Arguments)
+                            arg.Name = $"?{arg}";
 
-                macros.Add(new ReconstructionPair(macro, metaAction));
+                    if (macros.ContainsKey(metaAction))
+                        macros[metaAction].Add(groundedMacroSequence);
+                    else
+                        macros.Add(metaAction, new List<List<GroundedAction>>() { groundedMacroSequence });
+                }
             }
             ConsoleHelper.WriteLineColor("Done!", ConsoleColor.Green);
 
             ConsoleHelper.WriteLineColor("Outputting reconstruction data...");
-            IErrorListener listener = new ErrorListener();
-            ICodeGenerator<INode> codeGenerator = new PDDLCodeGenerator(listener);
-            int id = 1;
-            foreach (var macro in macros)
+            ICodeGenerator<ActionPlan> codeGenerator = new FastDownwardPlanGenerator(listener);
+            foreach(var key in  macros.Keys)
             {
-                var reconstructionString = $"; {macro.MetaAction}{Environment.NewLine}";
-                reconstructionString += codeGenerator.Generate(macro.Macro);
-                File.WriteAllText(Path.Combine(opts.OutputPath, $"repair{id++}.pddl"), reconstructionString);
+                PathHelper.RecratePath(Path.Combine(opts.OutputPath, key.ActionName));
+                int id = 1;
+                foreach (var replacement in macros[key])
+                    codeGenerator.Generate(new ActionPlan(replacement, replacement.Count), Path.Combine(opts.OutputPath, key.ActionName, $"sequence{id++}.plan"));
             }
             ConsoleHelper.WriteLineColor("Done!", ConsoleColor.Green);
         }
 
-        private static DomainDecl ParseDomain(string file)
+        private static int IndexOfMetaAction(ActionPlan leaderPlan)
         {
-            IErrorListener listener = new ErrorListener();
-            IParser<INode> parser = new PDDLParser(listener);
-            return parser.ParseAs<DomainDecl>(new FileInfo(file));
-        }
-
-        private static List<ActionDecl> GenerateSequence(List<GroundedAction> groundedActions, DomainDecl decl)
-        {
-            List<ActionDecl> actions = new List<ActionDecl>();
-            foreach (var groundedAction in groundedActions)
-                actions.Add(GenerateActionInstance(groundedAction, decl));
-            return actions;
-        }
-
-        private static ActionDecl GenerateActionInstance(GroundedAction action, DomainDecl decl)
-        {
-            ActionDecl target = decl.Actions.First(x => x.Name == action.ActionName).Copy();
-            var allNames = target.FindTypes<NameExp>();
-            for (int i = 0; i < action.Arguments.Count; i++)
+            int index = 0;
+            for (int i = 0; i < leaderPlan.Plan.Count; i++)
             {
-                var allRefs = allNames.Where(x => x.Name == target.Parameters.Values[i].Name).ToList();
-                foreach (var referene in allRefs)
-                    referene.Name = $"?{action.Arguments[i].Name}";
+                if (leaderPlan.Plan[i].ActionName.StartsWith("$meta"))
+                {
+                    index = i;
+                    break;
+                }
             }
-            return target;
+            return index;
         }
     }
 }
