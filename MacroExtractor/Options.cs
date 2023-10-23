040a38e66b4844eb3388d4d7720c700f711fac6b
Kristian Skov Johansen <kris701kj@gmail.com>
2023-10-23 14:16:41 +0200

    Initial project setup

---
+++ MacroExtractor/Options.cs
@@ -0,0 +1,19 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MacroExtractor
+{
+    public class Options
+    {
+        [Option("leader-plans", Required = true, HelpText = "Path the the leader plan files to extract macros from.")]
+        public IEnumerable<string> LeaderPlans { get; set; } = new List<string>();
+        [Option("follower-plans", Required = true, HelpText = "Path the the follower plan files to extract macros from.")]
+        public IEnumerable<string> FollowerPlans { get; set; } = new List<string>();
+        [Option("output", Required = false, HelpText = "Where to output macros.", Default = "output/train/replacementMacros")]
+        public string OutputPath { get; set; } = "output/train/replacementMacros";
+    }
+}
---
+++ MacroExtractor/Program.cs
@@ -0,0 +1,39 @@
+﻿using CommandLine;
+using System;
+using Tools;
+
+namespace MacroExtractor
+{
+    internal class Program : BaseCLI
+    {
+        static int Main(string[] args)
+        {
+            Parser.Default.ParseArguments<Options>(args)
+              .WithParsed(ExtractMacros)
+              .WithNotParsed(HandleParseError);
+            return 0;
+        }
+
+        public static void ExtractMacros(Options opts)
+        {
+            ConsoleHelper.WriteLineColor("");
+
+            List<FileInfo> leaderPlans = new List<FileInfo>();
+            foreach (var plan in opts.LeaderPlans)
+                leaderPlans.Add(new FileInfo(PathHelper.RootPath(plan)));
+            List<FileInfo> followerPlans = new List<FileInfo>();
+            foreach (var plan in opts.FollowerPlans)
+                followerPlans.Add(new FileInfo(PathHelper.RootPath(plan)));
+
+            List<PlanPair> planPairs = new List<PlanPair>();
+            foreach(var leaderPlan in leaderPlans)
+            {
+                var matches = followerPlans.Where(x => x.Name == leaderPlan.Name);
+                foreach (var match in matches)
+                    planPairs.Add(new PlanPair(leaderPlan, match));
+            }
+
+
+        }
+    }
+}
