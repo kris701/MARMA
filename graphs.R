7e9fd64cdf4ddc82867514ccfa0c80a1b51b97e5
Kristian Skov Johansen <kris701kj@gmail.com>
2023-11-06 13:24:25 +0100

    Added coverage graphs

--- graphs.R
+++ graphs.R
@@ -48,3 +48,68 @@ ggplot(combined, aes(x=totalTime.meta, y=totalTime.norm)) +
 		ylim=c(0, max(combined$totalTime.meta, combined$totalTime.norm)),
 	)
 dev.off()
+
+# Generate Coverage plot
+metaSearchTime <- lapply(list(combined$searchTime.meta), sort)[[1]]
+normSearchTime <- lapply(list(combined$searchTime.norm), sort)[[1]]
+highestValue <- max(metaSearchTime, normSearchTime)
+
+metaUnique <- unique(metaSearchTime)
+metaCounter <- c()
+normUnique <- unique(normSearchTime)
+normCounter <- c()
+
+dups <- duplicated(metaSearchTime)
+last <- 0
+for (i in 1:length(metaSearchTime)){
+	if (dups[i] == TRUE)
+	{
+		metaCounter[last] <- metaCounter[last] + 1
+	}
+	else
+	{
+		if (last == 0){
+			metaCounter[1] <- 1
+			last <- 1
+		} else {
+			last <- last + 1
+			metaCounter[last] <- metaCounter[last - 1] + 1
+		}
+	}
+}
+metaCoverageData <- data.frame(time=metaUnique, coverage=metaCounter)
+
+dups <- duplicated(normSearchTime)
+last <- 0
+for (i in 1:length(normSearchTime)){
+	if (dups[i] == TRUE)
+	{
+		normCounter[last] <- normCounter[last] + 1
+	}
+	else
+	{
+		if (last == 0){
+			normCounter[1] <- 1
+			last <- 1
+		} else {
+			last <- last + 1
+			normCounter[last] <- normCounter[last - 1] + 1
+		}
+	}
+}
+normCoverageData <- data.frame(time=normUnique, coverage=normCounter)
+
+jpeg(file="coverage.jpeg")
+ggplot() +
+	geom_line(data=metaCoverageData, aes(y=coverage,x= time,colour="With Reconstruction")) +
+	geom_line(data=normCoverageData, aes(y=coverage,x= time,colour="Without Reconstruction")) +
+	scale_color_manual(name = "Legend", values = c("With Reconstruction" = "red", "Without Reconstruction" = "blue")) +
+	ggtitle("Coverage") + 
+	xlab("Time") +
+	ylab("Problems Solved") + 
+	theme(text = element_text(size=15),
+		axis.text.x = element_text(angle=90, hjust=1)
+	)
+dev.off()
+
+
