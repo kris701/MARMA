c94f73ffbef8d9b440ba1386fdb2954d38b489c0
Kristian Skov Johansen <kris701kj@gmail.com>
2023-11-07 08:57:10 +0100

    Initial testing splitting

---
+++ MetaActions.Test/Reconstructors/IReconstructor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetaActions.Test.Reconstructors
+{
+    public interface IReconstructor : IDisposable
+    {
+        public Options.ReconstructionMethods Method { get; }
+
+        public string DomainName { get; }
+        public string ProblemName { get; }
+
+        public string Alias { get; }
+        public TimeSpan TimeLimit { get; }
+
+        public CancellationTokenSource CancellationToken { get; }
+        public Task<RunReport?> Run(CancellationTokenSource tokenSource);
+        public RunReport? Run();
+    }
+}
