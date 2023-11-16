67a9cff4e90de15f8f85678e187549d167a69d96
Kristian Skov Johansen <kris701kj@gmail.com>
2023-11-16 21:58:07 +0100

    Setup of testing scripts

---
+++ TestingSets/all_p01-p19-CSM + 360m + Extra.sh
@@ -0,0 +1,45 @@
+dotnet run --configuration Release --project MetaActions.Train -- \
+	--domains 			Dependencies/learning-benchmarks/*/domain.pddl \
+						Dependencies/downward-benchmarks/depot/domain.pddl \
+						Dependencies/downward-benchmarks/driverlog/domain.pddl \
+						Dependencies/downward-benchmarks/freecell/domain.pddl \
+						Dependencies/downward-benchmarks/gripper/domain.pddl \
+						Dependencies/downward-benchmarks/logistics98/domain.pddl \
+						Dependencies/downward-benchmarks/mprime/domain.pddl \
+						Dependencies/downward-benchmarks/mystery/domain.pddl \
+						Dependencies/downward-benchmarks/zenotravel/domain.pddl \
+	--train-problems 	Dependencies/learning-benchmarks/*/training/easy/p0*.pddl \
+						Dependencies/learning-benchmarks/*/training/easy/p1*.pddl \
+						Dependencies/downward-benchmarks/depot/p0*.pddl \
+						Dependencies/downward-benchmarks/depot/p1*.pddl \
+						Dependencies/downward-benchmarks/driverlog/p0*.pddl \
+						Dependencies/downward-benchmarks/driverlog/p1*.pddl \
+						Dependencies/downward-benchmarks/freecell/p0*.pddl \
+						Dependencies/downward-benchmarks/freecell/p1*.pddl \
+						Dependencies/downward-benchmarks/gripper/prob0*.pddl \
+						Dependencies/downward-benchmarks/gripper/prob1*.pddl \
+						Dependencies/downward-benchmarks/logistics98/prob0*.pddl \
+						Dependencies/downward-benchmarks/logistics98/prob1*.pddl \
+						Dependencies/downward-benchmarks/mprime/prob0*.pddl \
+						Dependencies/downward-benchmarks/mprime/prob1*.pddl \
+						Dependencies/downward-benchmarks/mystery/prob0*.pddl \
+						Dependencies/downward-benchmarks/mystery/prob1*.pddl \
+						Dependencies/downward-benchmarks/zenotravel/p0*.pddl \
+						Dependencies/downward-benchmarks/zenotravel/p1*.pddl \
+	--test-problems 	Dependencies/learning-benchmarks/*/testing/*/*.pddl \
+						Dependencies/downward-benchmarks/depot/p2*.pddl \
+						Dependencies/downward-benchmarks/driverlog/p2*.pddl \
+						Dependencies/downward-benchmarks/freecell/probfreecell*.pddl \
+						Dependencies/downward-benchmarks/gripper/prob2*.pddl \
+						Dependencies/downward-benchmarks/logistics98/prob2*.pddl \
+						Dependencies/downward-benchmarks/logistics98/prob3*.pddl \
+						Dependencies/downward-benchmarks/mprime/prob2*.pddl \
+						Dependencies/downward-benchmarks/mprime/prob3*.pddl \
+						Dependencies/downward-benchmarks/mystery/prob2*.pddl \
+						Dependencies/downward-benchmarks/mystery/prob3*.pddl \
+						Dependencies/downward-benchmarks/zenotravel/p2*.pddl \
+	--method CSMMacros \
+	--multitask \
+	--timelimit 360
+
+cp "output/train/*.zip" "TestingSets/all_p01-p19-CSM + 360m + Extra.zip"
--- TestingSets/all_p01-p19-PDDLSharp + 360m + Extra.sh
+++ TestingSets/all_p01-p19-PDDLSharp + 360m + Extra.sh
@@ -9,34 +9,37 @@ dotnet run --configuration Release --project MetaActions.Train -- \
 						Dependencies/downward-benchmarks/mystery/domain.pddl \
 						Dependencies/downward-benchmarks/zenotravel/domain.pddl \
 	--train-problems 	Dependencies/learning-benchmarks/*/training/easy/p0*.pddl \
+						Dependencies/learning-benchmarks/*/training/easy/p1*.pddl \
 						Dependencies/downward-benchmarks/depot/p0*.pddl \
-						Dependencies/downward-benchmarks/driverlog/p0*.pddl \
-						Dependencies/downward-benchmarks/freecell/p0*.pddl \
-						Dependencies/downward-benchmarks/gripper/prob0*.pddl \
-						Dependencies/downward-benchmarks/logistics98/prob0*.pddl \
-						Dependencies/downward-benchmarks/mprime/prob0*.pddl \
-						Dependencies/downward-benchmarks/mystery/prob0*.pddl \
-						Dependencies/downward-benchmarks/zenotravel/p0*.pddl \
-	--test-problems 	Dependencies/learning-benchmarks/*/testing/*/*.pddl \
 						Dependencies/downward-benchmarks/depot/p1*.pddl \
-						Dependencies/downward-benchmarks/depot/p2*.pddl \
+						Dependencies/downward-benchmarks/driverlog/p0*.pddl \
 						Dependencies/downward-benchmarks/driverlog/p1*.pddl \
-						Dependencies/downward-benchmarks/driverlog/p2*.pddl \
+						Dependencies/downward-benchmarks/freecell/p0*.pddl \
 						Dependencies/downward-benchmarks/freecell/p1*.pddl \
-						Dependencies/downward-benchmarks/freecell/probfreecell*.pddl \
+						Dependencies/downward-benchmarks/gripper/prob0*.pddl \
 						Dependencies/downward-benchmarks/gripper/prob1*.pddl \
-						Dependencies/downward-benchmarks/gripper/prob2*.pddl \
+						Dependencies/downward-benchmarks/logistics98/prob0*.pddl \
 						Dependencies/downward-benchmarks/logistics98/prob1*.pddl \
+						Dependencies/downward-benchmarks/mprime/prob0*.pddl \
+						Dependencies/downward-benchmarks/mprime/prob1*.pddl \
+						Dependencies/downward-benchmarks/mystery/prob0*.pddl \
+						Dependencies/downward-benchmarks/mystery/prob1*.pddl \
+						Dependencies/downward-benchmarks/zenotravel/p0*.pddl \
+						Dependencies/downward-benchmarks/zenotravel/p1*.pddl \
+	--test-problems 	Dependencies/learning-benchmarks/*/testing/*/*.pddl \
+						Dependencies/downward-benchmarks/depot/p2*.pddl \
+						Dependencies/downward-benchmarks/driverlog/p2*.pddl \
+						Dependencies/downward-benchmarks/freecell/probfreecell*.pddl \
+						Dependencies/downward-benchmarks/gripper/prob2*.pddl \
 						Dependencies/downward-benchmarks/logistics98/prob2*.pddl \
 						Dependencies/downward-benchmarks/logistics98/prob3*.pddl \
-						Dependencies/downward-benchmarks/mprime/prob1*.pddl \
 						Dependencies/downward-benchmarks/mprime/prob2*.pddl \
 						Dependencies/downward-benchmarks/mprime/prob3*.pddl \
-						Dependencies/downward-benchmarks/mystery/prob1*.pddl \
 						Dependencies/downward-benchmarks/mystery/prob2*.pddl \
 						Dependencies/downward-benchmarks/mystery/prob3*.pddl \
-						Dependencies/downward-benchmarks/zenotravel/p1*.pddl \
 						Dependencies/downward-benchmarks/zenotravel/p2*.pddl \
 	--method PDDLSharpMacros \
 	--multitask \
-	--rebuild
+	--timelimit 360
+	
+cp "output/train/*.zip" "TestingSets/all_p01-p19-PDDLSharp + 360m + Extra.zip"
