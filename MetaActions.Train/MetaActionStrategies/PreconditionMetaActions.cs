ca7b9435b6e6762d8a65a78e2a05dba3f1099cd7
Kristian Skov Johansen <kris701kj@gmail.com>
2023-11-24 22:14:43 +0100

    Fixed the precondition meta generator to consider all combinations of (non-static) preconditions

--- MetaActions.Train/MetaActionStrategies/PreconditionMetaActions.cs
+++ MetaActions.Train/MetaActionStrategies/PreconditionMetaActions.cs
@@ -54,34 +54,65 @@ namespace MetaActions.Train.MetaActionStrategies
             int metaCounter = 0;
             foreach(var action in domainDecl.Actions)
             {
-                if (action.Preconditions is AndExp and) 
+                if (action.Preconditions is AndExp and)
                 {
-                    for (int i = 0; i < and.Children.Count; i++)
+                    var permutations = GeneratePermutations(and, statics);
+                    foreach(var permutation in permutations)
                     {
-                        if (statics.Any(x => and.Children[i] is PredicateExp pred && x.Name == pred.Name))
-                            continue;
                         var copy = action.Copy();
                         copy.Name = $"$meta_{metaCounter++}";
-                        if (copy.Preconditions is AndExp copyAnd) {
-                            copyAnd.Children.RemoveAt(i);
+                        var copyPrecon = new AndExp();
+                        copy.Preconditions = copyPrecon;
+                        for (int i = 0; i < permutation.Length; i++)
+                            if (permutation[i] && and.Children[i].Copy() is IExp child)
+                                copyPrecon.Children.Add(child);
 
-                            var toRemove = new List<int>();
-                            for(int j = 0; j < copy.Parameters.Values.Count; j++)
-                            {
-                                var allRefs = copy.FindNames(copy.Parameters.Values[j].Name);
-                                if (allRefs.Count == 1)
-                                    toRemove.Add(j);
-                            }
-                            toRemove.Reverse();
-                            for (int j = 0; j < toRemove.Count; j++)
-                                copy.Parameters.Values.RemoveAt(toRemove[j]);
-
-                            metaActions.Add(copy);
+                        var toRemove = new List<int>();
+                        for (int j = 0; j < copy.Parameters.Values.Count; j++)
+                        {
+                            var allRefs = copy.FindNames(copy.Parameters.Values[j].Name);
+                            if (allRefs.Count == 1)
+                                toRemove.Add(j);
                         }
+                        toRemove.Reverse();
+                        for (int j = 0; j < toRemove.Count; j++)
+                            copy.Parameters.Values.RemoveAt(toRemove[j]);
+
+                        if (!copy.Preconditions.Equals(action.Preconditions))
+                            metaActions.Add(copy);
                     }
                 }
             }
             return metaActions;
+        }
+
+        private Queue<bool[]> GeneratePermutations(AndExp preconditions, List<PredicateExp> statics)
+        {
+            var queue = new Queue<bool[]>();
+            GeneratePermutations(preconditions, new bool[preconditions.Children.Count], 0, statics, queue);
+            return queue;
+        }
+
+        private void GeneratePermutations(AndExp preconditions, bool[] source, int index, List<PredicateExp> statics, Queue<bool[]> returnQueue)
+        {
+            var trueSource = new bool[source.Length];
+            Array.Copy(source, trueSource, source.Length);
+            trueSource[index] = true;
+            if (index < source.Length - 1)
+                GeneratePermutations(preconditions, trueSource, index + 1, statics, returnQueue);
+            else
+                returnQueue.Enqueue(trueSource);
+
+            if (!statics.Any(x => preconditions.Children[index] is PredicateExp pred && pred.Name == x.Name)) 
+            {
+                var falseSource = new bool[source.Length];
+                Array.Copy(source, falseSource, source.Length);
+                falseSource[index] = false;
+                if (index < source.Length - 1)
+                    GeneratePermutations(preconditions, falseSource, index + 1, statics, returnQueue);
+                else
+                    returnQueue.Enqueue(falseSource);
+            }
         }
 
         private List<FileInfo> OutputMetaActions(ICodeGenerator<INode> codeGenerator, List<ActionDecl> metaActions)
