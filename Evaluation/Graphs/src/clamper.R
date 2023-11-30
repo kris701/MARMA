45c5e3eb025eb839f81da04fb21667f9e3efa85a
Kristian Skov Johansen <kris701kj@gmail.com>
2023-11-30 13:30:45 +0100

    Updated graphs

--- Evaluation/Graphs/src/clamper.R
+++ Evaluation/Graphs/src/clamper.R
@@ -3,3 +3,9 @@ max_unsolved <- function(data, target) {
 	data[data$solved == "false",][,target] <- highest
 	return (data)
 }
+
+min_unsolved <- function(data, target) {
+	lowest <- min(data[,target], na.rm=TRUE)
+	data[data$solved == "false",][,target] <- lowest
+	return (data)
+}
